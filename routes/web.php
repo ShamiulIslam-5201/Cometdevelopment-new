ddd0076ae131c39fb49dd433f7799d1db00b1d6e
Shamiul Islam <Shamiul@gmail.com>
2021-04-10 15:57:26 +0600

    Post added successfully

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,13 @@ Route::post('admin/logout', [App\Http\Controllers\Auth\LoginController::class, '
 
 // post route
 Route::resource('post', 'App\Http\Controllers\PostController');
+
+
+// post category routes
 Route::resource('category', 'App\Http\Controllers\CategoryController');
 Route::get('category/status-inactive/{id}' , 'App\Http\Controllers\CategoryController@statusUpdateInactive');
 Route::get('category/status-active/{id}' , 'App\Http\Controllers\CategoryController@statusUpdateActive');
+
+
+// post Tag routes
+Route::resource('tag', 'App\Http\Controllers\TagController');
