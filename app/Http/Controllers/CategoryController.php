ef3d7de096304db434cb7857c1dadadae1d5416a
Shamiul Islam <Shamiul@gmail.com>
2021-04-10 14:54:55 +0600

    Edit feature for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -60,7 +60,7 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        //
+
     }
 
     /**
@@ -71,7 +71,13 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $edit_data = Category::find($id);
+
+        return [
+            'id' => $edit_data -> id,
+            'nama' => $edit_data -> name
+
+        ];
     }
 
     /**
@@ -83,7 +89,14 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $edit_id = $request -> edit_id;
+
+        $edit_data =  Category::find($edit_id);
+        $edit_data -> name = $request -> name;
+        $edit_data -> slug = Str::slug($request -> name);
+        $edit_data -> update();
+
+        return redirect() -> back() -> with('success', 'Category updated successful');
     }
 
     /**
